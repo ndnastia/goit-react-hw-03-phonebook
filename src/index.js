8a759fc59c0b1bdf68f5c367d40ecdaed812c6fc
ndnastia <ndnastiadatsiuk@gmail.com>
2023-10-04 23:48:24 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="">
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+<App />
+  </StrictMode>
+    
+ 
 );
